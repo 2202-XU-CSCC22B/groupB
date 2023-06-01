4317361f8b241209db84ba5d9c0b1cb309755299
Zenia-Gersana <2013001116@my.xu.edu.ph>
2023-06-01 09:25:01 +0000

    FIXED server (2)

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ db.once("open", function () {
 });
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://groupB.onrender.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://groupb.onrender.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
